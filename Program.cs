1a57ceea99b166ada7cb69ecbade94f24eb4b1b1
Renato <rossorene4@gmail.com>
2023-03-03 22:51:49 -0300

    delícia

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Minhas frutas preferidas");
+Console.WriteLine("");
+Console.WriteLine("1.maça");
+Console.WriteLine("2.banana");
+Console.WriteLine("3.manga");
+Console.WriteLine("4.pessego");
+Console.WriteLine("5.melancia");
